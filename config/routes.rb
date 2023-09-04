907931ccd05dc7c425623225454d8f19510f584e
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-09-04 13:46:13 -0600

    Add view for foods

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
   end
   get 'home/index'
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show, :destroy]
+  resources :foods, only: [:new, :create, :index, :destroy]
 end
