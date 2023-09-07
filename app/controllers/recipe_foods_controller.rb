923877787f103b9b31b993272424a9140ae6a21f
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-09-07 14:49:32 -0600

    Fix errors

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -20,9 +20,42 @@ class RecipeFoodsController < ApplicationController
     end
   end
 
+  def edit
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = RecipeFood.find(params[:id])
+end
+
+def update
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = RecipeFood.find(params[:id])
+
+    if @recipe_food.update(recipe_food_params)
+        flash[:notice] = 'Ingredient updated successfully'
+        redirect_to recipe_path(@recipe)
+    else
+        flash[:alert] = 'Ingredient failed to update'
+        render :edit
+    end
+end
+
+def destroy
+  @recipe = Recipe.find(params[:recipe_id])
+  @recipe_food = RecipeFood.find(params[:id])
+
+  if @recipe_food.destroy
+      flash[:notice] = 'Ingredient deleted successfully'
+  else
+      flash[:alert] = 'Ingredient failed to delete'
+  end
+
+  redirect_to recipe_path(@recipe)
+end
+
+
+
   private
 
   def recipe_food_params
-    params.require(:recipe_food).permit(:quantity, :food_id)
+    params.require(:recipe_food).permit(:food_id, :quantity)
   end
 end
