9fff85a86ee6de33786e54e9ed264ceb267e51b9
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-09-09 08:31:28 -0600

    fix puma

--- config/puma.rb
+++ config/puma.rb
@@ -39,7 +39,7 @@ workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-reload_app!
+preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
